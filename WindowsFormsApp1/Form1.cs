0be44b43ece41e718a6096b19c5f80d4bfc06cf2
Paško Berišić <paberisi@vsite.hr>
2020-06-08 19:36:59 +0200

    DONE !!

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -9,19 +9,25 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            MultitonLogic instance = new MultitonLogic();
             this.Hide();
             Login login = new Login();
+           // instance.UpdateBase();
             login.ShowDialog();
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            MultitonLogic instance = new MultitonLogic();
             this.Hide();
             Register register = new Register();
+           // instance.UpdateBase();
             register.ShowDialog();
         }
     }
--- WindowsFormsApp1/MultitonLogic.cs
+++ WindowsFormsApp1/MultitonLogic.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -34,57 +35,86 @@ namespace WindowsFormsApp1
         StreamReader sr = new StreamReader(@"C:\Users\Korisnik\source\repos\Multiton-SignUp-SignIn\config.txt");
         StreamWriter sw = new StreamWriter(@"C:\Users\Korisnik\source\repos\Multiton-SignUp-SignIn\config.txt", true);
         */
-        public bool AddUser(string username, string password)
+        /*
+        public void UpdateBase()
         {
             StreamReader sr = new StreamReader(@"C:\Users\Pasko\Source\Repos\PaskoBerisic\Multiton-SignUp-SignIn\config.txt");
+            StreamWriter sw = new StreamWriter(@"C:\Users\Pasko\Source\Repos\PaskoBerisic\Multiton-SignUp-SignIn\username's.txt",true);
+            string temporary = sr.ReadLine();
+            int index = 0;
+            while (temporary != null)
+            {
+                if (temporary != "-")
+                {
+                    if (index > 0)
+                    {
+                        index = temporary.IndexOf("|");
+                        temporary = temporary.Substring(0, index);
+                        sw.WriteLine(temporary);
+                        sw.WriteLine("-");
+                        sw.Flush();
+                        
+                    }
+                }
+                temporary = sr.ReadLine();
+            }
+            sw.Close();
+            sr.Dispose();
+            sr.Close();
+        }*/
+
+        public bool AddUser(string username, string password)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\Pasko\Source\Repos\PaskoBerisic\Multiton-SignUp-SignIn\username's.txt");
             //provjera korisnika je li u bazi
             // ako je, return flase
             //else dodaj novog, return true
             string temporary = sr.ReadLine();
             while (temporary != null)
             {
-                StreamWriter sw = new StreamWriter(@"C:\Users\Pasko\Source\Repos\PaskoBerisic\Multiton-SignUp-SignIn\config.txt",true);
+                StreamWriter sw = new StreamWriter(@"C:\Users\Pasko\Source\Repos\PaskoBerisic\Multiton-SignUp-SignIn\config.txt", true);
                 //Check if username exist
                 if (temporary == username)
                     break;
                 else
-                    sr.Close();
                 sw.Write(username);
-                    sw.Write("|");
-                    sw.WriteLine(password);
-                    sw.WriteLine("-");
-                    sw.Flush();
-                    sw.Close();
+                sw.Write("|");
+                sw.WriteLine(password);
+                sw.WriteLine("-");
+                sw.Flush();
+                sw.Close();
                 //Read the next line
                 temporary = sr.ReadLine();
             }
             return false;
-          }
-          public bool GetUser(string username)
-          {
-                StreamReader sr = new StreamReader(@"C:\Users\Pasko\Source\Repos\PaskoBerisic\Multiton-SignUp-SignIn\config.txt");
-                //for provjera postojio li username
-                // ako postoji, vrati usera else null
-                User user = new User();
-                string temporary = sr.ReadLine();
-                int index = 0;
-                
-                while (temporary != null)
-                {
+        }
+        public bool GetUser(string username)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\Pasko\Source\Repos\PaskoBerisic\Multiton-SignUp-SignIn\config.txt");
+            //for provjera postojio li username
+            // ako postoji, vrati usera else null
+            User user = new User();
+            string temporary = sr.ReadLine();
+            int index = 0;
+
+            while (temporary != null)
+            {
                 //Remove everything after splitter
                 index = temporary.IndexOf("|");
                 if (index > 0)
                     temporary = temporary.Substring(0, index);
                 //Check if username exist
                 if (temporary == username)
-                        return true;
-                    //Read the next line
-                    temporary = sr.ReadLine();
-                }
-                sr.Dispose();
-                sr.Close();
+                    return true;
+                //Read the next line
+                temporary = sr.ReadLine();
+                
+            }
+            sr.Dispose();
+            sr.Close();
+            
             return false;
         }
-      
+
     }
 }
